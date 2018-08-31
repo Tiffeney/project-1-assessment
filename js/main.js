a26077b95924409116eafbd20358b335e88d2058
Tiffeney <tiffeney.rambo@gmail.com>
2018-08-31 16:56:18 -0700

    finish assessment

---
+++ js/main.js
@@ -0,0 +1,17 @@
+console.log(`JS LOADED`)
+
+let addButton = document.querySelector('#plus-btn');
+let subButton = document.querySelector('#minus-btn');
+// let input = documentx.querySelector('input');
+// let count = count++1;
+let counter = 0;
+
+
+
+addButton.addEventListener('click', function() {
+   document.getElementById('plus-btn').innerHTML = value
+})
+
+subButton.addEventListener('click', function() {
+    document.getElementById('minus-btn').innerHTML = value
+})
